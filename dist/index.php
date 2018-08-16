acf75773be3a78fc056ad6aa3012886773ab4edc
Esteban Martinez <fullstackmartinez@gmail.com>
2018-08-16 14:51:19 -0600

    wrote html markup for nav and header/hero image
    started sass files with primary/sec colors, global css such as body font/height and headings

--- dist/index.php
+++ dist/index.php
@@ -5,10 +5,61 @@
 		<meta name="viewport"
 				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/main.css">
 		<title>Esteban Martinez</title>
 	</head>
 	<body>
+		<header>
+			<div class="menu-btn">
+				<div class="btn-line"></div>
+				<div class="btn-line"></div>
+				<div class="btn-line"></div>
+			</div>
 
+			<nav class="menu">
+				<div class="menu-branding">
+					<div class="portrait"></div>
+				</div>
+				<ul class="manu-nav">
+					<li class="nav-item">
+						<a href="/" class="nav-link">Home</a>
+					</li>
+					<li class="nav-item">
+						<a href="/about.php" class="nav-link">About</a>
+					</li>
+					<li class="nav-item">
+						<a href="/project.php" class="nav-link">Projects</a>
+					</li>
+					<li class="nav-item">
+						<a href="/contact.php" class="nav-link">Reach Me</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+
+		<main id="home">
+			<h1 class="lg-heading">
+				John <span class="text-secondary">Doe</span>
+			</h1>
+			<h2 class="sm-heading">
+				Web Developer and Designer
+			</h2>
+			<div class="icons">
+				<a href="#">
+					<i class="fab fa-twitter fa-2x"></i>
+				</a>
+				<a href="#">
+					<i class="fab fa-facebook fa-2x"></i>
+				</a>
+				<a href="#">
+					<i class="fab fa-linkedin fa-2x"></i>
+				</a>
+				<a href="#">
+					<i class="fab fa-github fa-2x"></i>
+				</a>
+			</div>
+		</main>
+		<script src="js/main.js"
 	</body>
 </html>
