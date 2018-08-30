4f4788e76b29b47803086c6d9df0937d7e563a11
Esteban Martinez <fullstackmartinez@gmail.com>
2018-08-30 16:14:56 -0600

    added @mixins with media queries for breakpoints, make main and menu overlay responsive

--- dist/index.php
+++ dist/index.php
@@ -26,7 +26,7 @@
 						<a href="/" class="nav-link">Home</a>
 					</li>
 					<li class="nav-item">
-						<a href="/about.php" class="nav-link">About</a>
+						<a href="about.html" class="nav-link">About</a>
 					</li>
 					<li class="nav-item">
 						<a href="/project.php" class="nav-link">Projects</a>
